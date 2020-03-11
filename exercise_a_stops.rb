704845c59f88bf48a35ed3da6be23126f0cd23b7
Tony Tang <teokigi@hotmail.com>
2020-03-11 16:56:14 +0000

    print index of linlithgow

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -6,8 +6,8 @@ stops.push("Edinburgh Waverley")
 stops.unshift("Glasgow queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
-p stops
 #4. Print out the index position of "Linlithgow"
+p stops.index("Linlithgow")
 #5. Remove "Livingston" from the array using its name
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
