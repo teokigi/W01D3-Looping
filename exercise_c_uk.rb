1e8c72c335b8e33f3fddb4f05592161822a87672
Tony Tang <teokigi@hotmail.com>
2020-03-11 18:42:34 +0000

    changed loop option for 3

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -24,10 +24,13 @@ p "capital of wales is #{united_kingdom[1][:capital]}"
 united_kingdom.push({name: "Northern Ireland", population: 1811000, capital: "Belfast"})
 p "added new information for #{united_kingdom[3][:name]},popul:#{united_kingdom[3][:population]},capital:#{united_kingdom[3][:capital]}"
 # 3. Use a loop to print the names of all the countries in the UK.
-count = 0
-while count < united_kingdom.length
-  p united_kingdom[count][:name]
-  count += 1
+# count = 0
+# while count < united_kingdom.length
+#   p united_kingdom[count][:name]
+#   count += 1
+# end
+for countries in united_kingdom
+  p countries[:name]
 end
 
 # 4. Use a loop to find the total population of the UK.
