778f28d41b62fc0f86108a1dc39186002a51bd5a
Tony Tang <teokigi@hotmail.com>
2020-03-11 17:07:10 +0000

    P #of stops

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -12,8 +12,9 @@ p stops.index("Linlithgow")
 stops.delete("Livingston")
 #6. Delete "Cumbernauld" from the array by index
 stops.delete_at(2)
-p stops
 #7. Print the number of stops there are in the array?
+p stops.length
+p stops
 #8. Show as many ways as you can to return "Falkirk High" from the array?
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
