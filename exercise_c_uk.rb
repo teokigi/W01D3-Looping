6514d606f0201e912c096b8557804fe538ecf61d
Tony Tang <teokigi@hotmail.com>
2020-03-11 18:30:50 +0000

    +country data, +for loop puts

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -19,6 +19,12 @@ united_kingdom = [
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 united_kingdom[1][:capital]= "Cardiff"
 p "capital of wales is #{united_kingdom[1][:capital]}"
-# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array
+#(The capital is Belfast, and the population is 1,811,000).
+united_kingdom.push({name: "Northern Ireland", population: 1811000, capital: "Belfast"})
+p "added new information for #{united_kingdom[3][:name]},popul:#{united_kingdom[3][:population]},capital:#{united_kingdom[3][:capital]}"
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p united_kingdom[country]
+end
 # 4. Use a loop to find the total population of the UK.
