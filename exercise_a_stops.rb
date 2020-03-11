da3ce0869ece1d027dff9edff97cdd14f9602c0f
Tony Tang <teokigi@hotmail.com>
2020-03-11 16:46:12 +0000

    ex_a added Edinburgh stop

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -1,6 +1,8 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 #1. Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+p stops
 #2. Add "Glasgow Queen St" to the start of the array
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 #4. Print out the index position of "Linlithgow"
