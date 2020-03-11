3067dce1c74f07a19d4f7b74e97a3b0c15528688
Tony Tang <teokigi@hotmail.com>
2020-03-11 17:44:33 +0000

    find avrils pet species

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -61,6 +61,7 @@ p users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
 p "Eriks Lottery numbers are: #{users["Erik"][:lottery_numbers]}"
 # 4. Get the species of Avril's pet Monty
+p "Avril's pet Monty is a species of #{users["Avril"][:pets][0][:species]}"
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
