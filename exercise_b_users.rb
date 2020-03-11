6ade3c0773eb7cddb6730a5d4e7a604635272313
Tony Tang <teokigi@hotmail.com>
2020-03-11 17:36:15 +0000

    get johnnys twit handle exit

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,6 +55,7 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
 # 3. Get the array of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
