c73364bae4c622d2eff3f8305423489dda45bf35
Tony Tang <teokigi@hotmail.com>
2020-03-11 17:46:14 +0000

    Erik's smallest lotto number

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -63,6 +63,7 @@ p "Eriks Lottery numbers are: #{users["Erik"][:lottery_numbers]}"
 # 4. Get the species of Avril's pet Monty
 p "Avril's pet Monty is a species of #{users["Avril"][:pets][0][:species]}"
 # 5. Get the smallest of Erik's lottery numbers
+p "Erik's smallest lotto # is #{users["Erik"][:lottery_numbers].min}"
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
